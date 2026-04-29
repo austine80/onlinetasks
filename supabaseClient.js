a5722cf874bae933af6f83954a4ecca8d7af1e87
Austine <austineokoth80@gmail.com>
2026-04-30 02:32:41 +0300

    replacing supabase with supabaseclient

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,5 @@
 const supabaseUrl = "https://ypswtqxlvxfccanrlocq.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwc3d0cXhsdnhmY2NhbnJsb2NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2ODM0NzMsImV4cCI6MjA5MjI1OTQ3M30.wMLMV3STIMutXGLXSIjGYdFgalkkg9zoM-JCM0NtC-0";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const { createClient } = window.supabase;
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
